b2677646a60b0845da59ce607af92507a4555fbe
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-20 07:56:12 -1000

    fix check for second run function to include run name and new format

--- src/components/HabitifyCards/RunningCard.jsx
+++ src/components/HabitifyCards/RunningCard.jsx
@@ -47,9 +47,13 @@ export default function RunningCard({ rawRunningData, currentDate }) {
 
     function checkForSecondRun(data) {
         if (data.distance2) {
-            return `<br />[ ${convertMetersToMiles(
-                data.distance2
-            )} miles ] <br />[ ${convertTime(data.time2)} ]`
+            return `
+            <br />[ Second Run ]
+            <br />[ ${convertMetersToMiles(
+        data.distance2
+    )} miles ] <br />[ ${convertTime(data.time2)} ]
+    <br />[ ${data.name2} ]
+    `
         } else {
             return ``
         }
